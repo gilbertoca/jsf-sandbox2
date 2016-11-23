aa69856bb7e3caa8c3fc32be55b144a11d7c62e7
Gilberto Caetano de Andrade <gilbertoca@gmail.com>
2016-11-23 18:25:54 -0300

    Ajustes no converter CNPJ

--- src/main/java/jsf/sandbox/CNPJConverter.java
+++ src/main/java/jsf/sandbox/CNPJConverter.java
@@ -1,34 +1,40 @@
 package jsf.sandbox;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
 import javax.faces.convert.FacesConverter;
 import org.jrimum.domkee.comum.pessoa.id.cprf.AbstractCPRF;
-import org.jrimum.vallia.AbstractCPRFValidator;
-import static org.jrimum.vallia.AbstractCPRFValidator.TipoDeCPRF.CNPJ;
+import org.jrimum.domkee.comum.pessoa.id.cprf.CNPJ;
 
 @FacesConverter(value = "cnpjConverter")
 public class CNPJConverter implements Converter {
 
     @Override
-    public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        if (value == null || value.trim().isEmpty()) {
+    public Object getAsObject(FacesContext context, UIComponent component, String submittedValue) {
+        if (submittedValue == null || submittedValue.isEmpty()) {
             return null;
         }
-        new CNPJ();
-        AbstractCPRFValidator.isParametrosValidos(value, CNPJ)
-        return AbstractCPRF.create(value);
 
-			FacesMessage msg =
-				new FacesMessage("URL Conversion error.",
-						"Invalid URL format.");
-			msg.setSeverity(FacesMessage.SEVERITY_ERROR);
-			throw new ConverterException(msg);        
+        try {
+            return AbstractCPRF.create(submittedValue);
+        } catch (IllegalArgumentException e) {
+            throw new ConverterException(new FacesMessage(submittedValue + " não é um CNJP válido"), e);
+        }
     }
 
     @Override
-    public String getAsString(FacesContext context, UIComponent component, Object value) {
-        return (String) value;
+    public String getAsString(FacesContext context, UIComponent component, Object modelValue) {
+        if (modelValue == null) {
+            return "";
+        }
+
+        if (modelValue instanceof CNPJ) {
+            return String.valueOf(((CNPJ) modelValue).toString());
+        } else {
+            throw new ConverterException(new FacesMessage(modelValue + " não é um CNJP válido"));
+        }
     }
 }
